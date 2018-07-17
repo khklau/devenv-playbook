58538e05a1f36f20d3e509a6e6a85328de981a69
Kean Lau <khklau@users.noreply.github.com>
2018-07-17 22:25:13 +1000

    on Centos 7 we need to explicity specify the assembler because CMake has problems finding it

--- clang.sh
+++ clang.sh
@@ -9,11 +9,13 @@ mkdir -p ${HOME}/opt/clang-6.0.0
     wget http://releases.llvm.org/6.0.0/compiler-rt-6.0.0.src.tar.xz
     tar -xvJf ./llvm-6.0.0.src.tar.xz
     tar -xvJf ./cfe-6.0.0.src.tar.xz
+    tar -xvJf ./compiler-rt-6.0.0.src.tar.xz
+    mv llvm-6.0.0.src llvm
     mv cfe-6.0.0.src llvm/tools/clang
     mv compiler-rt-6.0.0.src llvm/projects/compiler-rt
     mkdir build
     cd build
-    cmake -DCMAKE_INSTALL_PREFIX=${HOME}/opt/clang-6.0.0 -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_BUILD_LLVM_DYLIB=ON -G "Unix Makefiles" ../llvm
+    ASM=as cmake3 -DCMAKE_INSTALL_PREFIX=${HOME}/opt/clang-6.0.0 -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_BUILD_LLVM_DYLIB=ON -G "Unix Makefiles" ../llvm
     make install
 )
 cd "${HERE}"
