a065623e15876109c1f554f5aa4ff2813931c491
Kean Lau <khklau@users.noreply.github.com>
2018-07-17 22:26:05 +1000

    adding extra tools

--- base_tools.sh
+++ base_tools.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+# Binary tools
+yum install binutils
+
 # Source tools
 yum install git
 
 # Build tools
-yum install cmake3
+yum install autoconf automake make cmake3
+
+# Library tools
+yum install libtool pkgconfig
 
 # Scripting tools
 yum install python
+
+# Misc tools
+yum install patch
