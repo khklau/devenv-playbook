3a871540628512d8e5885a6f8345f9e4d4d949d4
Kean Lau <khklau@users.noreply.github.com>
2018-07-21 11:26:55 +1000

    the std parameter for clang is -std not --std

--- cquery.sh
+++ cquery.sh
@@ -11,7 +11,7 @@ mkdir -p ${HOME}/opt
     cd build
     (
 	export CXX="/home/kean/opt/clang-6.0.0/bin/clang"
-	export CXXFLAGS="--std=c++14 -Wl,--rpath,/home/kean/opt/clang-6.0.0/lib"
+	export CXXFLAGS="-std=c++14 -Wl,--rpath,/home/kean/opt/clang-6.0.0/lib"
 	cmake .. -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${HOME}/opt/cquery-v20180718 -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DSYSTEM_CLANG=YES -DCMAKE_PREFIX_PATH="/home/kean/opt/clang-6.0.0"
 	make install
     )
